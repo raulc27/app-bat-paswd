f6c031ad5b92237ddacac840328fdf0d0e7d4f4b
Raul Castro <raul.de.almeida.neto@accenture.com>
2025-08-19 21:17:25 -0300

    refact

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -3,8 +3,8 @@ import { StatusBar } from 'expo-status-bar';
 import {
     View,
     Text,
-    StyleSheet
 } from 'react-native';
+import styles from './Style';
 
 export default function Home(){
     return(
@@ -17,11 +17,3 @@ export default function Home(){
     )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
